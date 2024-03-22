2095118c4b7900622fa4de096febc49e1fe498ed
iammithun <157653522+iammithun@users.noreply.github.com>
2024-03-22 19:12:26 -0400

    Add files via upload

---
+++ tkinter.py
@@ -0,0 +1,16 @@
+
+import tkinter as Tk
+
+
+window=Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+
+# lable
+
+my_lable=tkinter.Label(text="I Am a label", font=("aril", 24, "bold"))
+my_lable.pack()
+
+
+
+
