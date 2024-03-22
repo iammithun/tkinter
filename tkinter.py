4383b586609a78367fcf3c99b567011ecdb864e9
iammithun <157653522+iammithun@users.noreply.github.com>
2024-03-22 19:12:59 -0400

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -2,7 +2,7 @@
 import tkinter as Tk
 
 
-window=Tk()
+window=Tk
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
@@ -12,5 +12,8 @@ my_lable=tkinter.Label(text="I Am a label", font=("aril", 24, "bold"))
 my_lable.pack()
 
 
+import turtle
+tim=turtle.Turtle()
 
+tim.write()
 
